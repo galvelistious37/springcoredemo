19b68a94c2999eaa02f8ceb0069038cfa9b00999
johnny <john.robertson88@yahoo.com>
2024-03-25 20:17:36 -0700

    Initial commit

---
+++ src/main/java/com/johnny/spring/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.johnny.spring.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnny/spring/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnny.spring.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
